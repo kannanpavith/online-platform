ace28f9a6cf1a841a1ff32e87587e10decbae013
kannanpavith <147411864+kannanpavith@users.noreply.github.com>
2024-07-02 13:34:12 +0530

    courses.js

---
+++ models/courses.js
@@ -0,0 +1,33 @@
+// models/course.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Course = sequelize.define('Course', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  instructor_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+});
+
+module.exports = Course;
